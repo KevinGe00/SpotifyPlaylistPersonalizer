bcc3f06349c0d9d4f73b74994dc1e42b9ec9b359
Kevin Ge <kevinge@Kevins-MacBook-Pro.local>
2019-09-12 17:14:51 -0400

    html redesign

--- authorization_code/public/script.js
+++ authorization_code/public/script.js
@@ -1,3 +1,7 @@
 $('#login').on("click", function(){
-    window.alert("After logging in, check your spotify for your brand new playlist waiting for you!");
+    window.alert("After logging in, check your spotify for your brand new playlist waiting for you!np");
+})
+
+$(".overlay").on('click', function(){
+    window.alert('d');
 })
